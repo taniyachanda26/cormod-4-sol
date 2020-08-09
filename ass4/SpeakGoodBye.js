5e5296f55f4e3c53f01d4c5ffc48e81d29af6858
taniyachanda26 <64738232+taniyachanda26@users.noreply.github.com>
2020-08-09 10:55:40 +0530

    Update SpeakGoodBye.js

--- ass4/SpeakGoodBye.js
+++ ass4/SpeakGoodBye.js
@@ -1,7 +1,7 @@
 (function(window){
 var speakWord = "Good Bye";
 var byeSpeaker={};
-byeSpeaker.speakhello=function(name) {
+byeSpeaker.speak=function(name) {
     console.log(speakWord + " " + name);
 }
     window.byeSpeaker=byeSpeaker;
