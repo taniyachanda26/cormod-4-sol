3da17b9ef8f713d8b213067fc09e8eaac7fb89b2
taniyachanda26 <64738232+taniyachanda26@users.noreply.github.com>
2020-08-09 10:59:26 +0530

    Update SpeakHello.js

--- ass4/SpeakHello.js
+++ ass4/SpeakHello.js
@@ -1,8 +1,8 @@
+(function(window){
 var speakWord = "Hello";
-
-// STEP 4: Rewrite the 'speak' function such that it is attached to the
-// helloSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-function speak(name) {
+var helloSpeaker={};
+helloSpeaker.speak=function(name) {
     console.log(speakWord + " " + name);
 }
+window.helloSpeaker=helloSpeaker;
+})(window);
